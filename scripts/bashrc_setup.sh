a5225a262f5a5fba12479a4352cd092e481f3008
Luigi Raiano <raiano.luigi@gmail.com>
2023-03-23 18:05:21 +0100

    removed source to barshrc

--- scripts/bashrc_setup.sh
+++ scripts/bashrc_setup.sh
@@ -7,7 +7,6 @@ echo "Adding personalization to bashrc..."
 sudo echo "" >> ~/.bashrc
 sudo echo "source $CUSTOM_SCRIPTS_DIR/scripts/custom_aliases.sh" >> ~/.bashrc
 sudo echo "source $CUSTOM_SCRIPTS_DIR/scripts/lr_terminal_appearance.sh" >> ~/.bashrc
-sudo echo "source $CUSTOM_SCRIPTS_DIR/scripts/env_params.env" >> ~/.bashrc
 
 echo ""
 echo "bashrc personalization completed."
