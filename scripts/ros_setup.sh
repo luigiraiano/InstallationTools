7aaa3b929ae1e179ad6f57e7b789bcc79850ae96
Luigi Raiano <raiano.luigi@gmail.com>
2023-03-23 21:59:16 +0100

    removed installation of developer libraries and moved in a dedicated script

---
+++ scripts/ros_setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+CUSTOM_SCRIPTS_DIR=$GITHUB_DIR/Utils/custom_scripts
+
+echo ""
+echo "Installing ROS Noetic ..."
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt install curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo apt update
+sudo apt install ros-noetic-desktop-full
+
+echo ""
+echo "ROS Noetic Installed. Installing main dependencies ..."
+
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+sudo apt install python3-rosdep
+sudo rosdep init
+rosdep update
+
+echo ""
+echo "Installing Qt-ROS Plugin ..."
+sudo snap install qtcreator-ros --classic
+
+
+echo ""
+echo "personalizing bashrc ..."
+
+sudo echo "" >> ~/.bashrc
+sudo echo "source $CUSTOM_SCRIPTS_DIR/scripts/set_ros_master_uri.sh" >> ~/.bashrc
+
+echo ""
+echo "bashrc personalization completed."
