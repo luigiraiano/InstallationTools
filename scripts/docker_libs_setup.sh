68378b8a59f93578bd5c3ea9680ea20a40d9a9a1
Luigi Raiano <raiano.luigi@gmail.com>
2024-03-01 16:15:17 +0100

    renamed docker libs installation file

--- scripts/docker_libs_setup.sh
+++ scripts/docker_libs_setup.sh
@@ -3,31 +3,35 @@
 # Script generated from official Docker Repo: https://docs.docker.com/engine/install/ubuntu/
 
 # 01
-## Add Docker's official GPG key:
+# Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-## Add the repository to Apt sources:
+# Add the repository to Apt sources:
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
 # 02 - Install Docker Packages
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugins
+sleep 5
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # 03 - Testing Docker Installation
+sleep 5
 sudo docker run hello-world
 
 # 04 - Remove the need of using SUDO
 ## Create the docker group.
+sleep 5
 sudo groupadd docker
 
 ## Add your user to the docker group.
+sleep 5
 sudo usermod -aG docker $USER
 
 ## INFO
