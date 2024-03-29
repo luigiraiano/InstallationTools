5b678a79d9eb0f61b0d5173a272e9460666e1b0c
Luigi Raiano <raiano.luigi@gmail.com>
2024-03-29 16:01:43 +0100

    add catkin tools installation

--- scripts/ros_setup.sh
+++ scripts/ros_setup.sh
@@ -41,6 +41,18 @@ rosdep update
 echo ""
 echo "Installing Qt-ROS Plugin ..."
 sudo snap install qtcreator-ros --classic
+echo "Qt-ROS Installed"
+
+echo ""
+echo "Installing Catkin Tools..."
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list'
+wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install python3-catkin-tools
+
+echo ""
+echo "Catkin Tools Installed"
 
 echo ""
 echo "personalizing bashrc ..."
